37b3cf2fe8af5d7994984e21fe92f72fc3e2b557
Eric Conklin <CONKLIE3@ccf.org>
2022-03-31 08:38:24 -0400

    add initial files

---
+++ Assignment6.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Assignment6 {
+
+    public static void main(final String[] args) throws IOException {
+        System.out.println("Hello world - this is assignment 6 in Java!");
+        System.out.println("Let's create a linked list and start adding to it.");
+
+        System.out.println("That's all. Thanks for joining me!");
+    }
+
+}
